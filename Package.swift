0f2d47e9442500ba0c409f54d65862f8681d4ca8
Dominic Egginton <dominic.egginton@gmail.com>
2021-01-18 17:49:18 +0000

    added flag for coin flip animation

--- Package.swift
+++ Package.swift
@@ -5,11 +5,13 @@ import PackageDescription
 let package = Package(
     name: "flip",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(url: "https://github.com/dominicegginton/Spinner", from: "1.0.0")
     ],
     targets: [
         .target(name: "flip", dependencies: [
-            .product(name: "ArgumentParser", package: "swift-argument-parser")
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            "Spinner"
         ])
     ]
 )
--- Sources/flip/main.swift
+++ Sources/flip/main.swift
@@ -1,18 +1,28 @@
 import ArgumentParser
+import Spinner
+import Foundation
 
 struct Flip: ParsableCommand {
 
     @Option(name: .shortAndLong, help: "The number of coins to repeat flip.")
     var coins: Int?
 
+    @Flag(name: .shortAndLong, help: "Animated output")
+    var animation: Bool = false
+
     mutating func run() throws {
         let count = coins ?? 1
 
         for _ in 1...count {
+            let spinner = Spinner(.flip, "")
+            if animation {
+                spinner.start()
+                sleep(1)
+            }
             if Bool.random() {
-                print("heads")
+                spinner.stop(finalFrame: "•", text: "heads")
             } else {
-                print("tails")
+                spinner.stop(finalFrame: "•", text: "tails")
             }
         }
     }
