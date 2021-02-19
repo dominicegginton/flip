6d080e6f8b0a18081eaca92c6c10ad7e26808102
Dominic Egginton <dominic.egginton@gmail.com>
2021-02-19 12:07:49 +0000

    render colored and animated output as default

--- Sources/flip/main.swift
+++ Sources/flip/main.swift
@@ -6,26 +6,24 @@ struct Flip: ParsableCommand {
     @Option(name: .shortAndLong, help: "The number of coins to flip.")
     var coins: Int?
 
-    @Flag(name: .shortAndLong, help: "Animated output.")
-    var animation: Bool = false
-
-    @Flag(name: .long, help: "Colored output.")
-    var color: Bool = false
+    @Flag(name: .shortAndLong, help: "Disable animated and colored output.")
+    var disable: Bool = false
 
     mutating func run() throws {
         for _ in 1 ... (coins ?? 1) {
-            let spinner = Spinner(.flip, "")
-            if color {
-                spinner.updateColor(.yellow)
+            let result: String
+            if Bool.random() {
+                result = "heads"
+            } else {
+                result = "tails"
             }
-            if animation {
+            if (disable) {
+                print(result)
+            } else {
+                let spinner = Spinner(.flip, "", color: .yellow)
                 spinner.start()
                 sleep(1)
-            }
-            if Bool.random() {
-                spinner.stop(finalFrame: "•", text: "heads")
-            } else {
-                spinner.stop(finalFrame: "•", text: "tails")
+                spinner.stop(finalFrame: "•", text: result)
             }
         }
     }
